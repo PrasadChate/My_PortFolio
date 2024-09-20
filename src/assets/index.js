8db15f7378326e7d0ec7cb22d5849d337afde377
Prasad Chate <118675338+PrasadChate@users.noreply.github.com>
2024-09-20 19:11:03 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
+import AI from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
@@ -34,7 +34,7 @@ import tripguide from "./tripguide.png";
 export {
   logo,
   backend,
-  creator,
+  AI,
   mobile,
   web,
   github,
